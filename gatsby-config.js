08c501ea14c12af435286590d078557fe89f0c46
Gustavo Paiva <gustavorpaiva@gmail.com>
2022-02-02 16:17:12 -0300

    fixes syntax

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
-    templateKey: blog-post
-title: "Gatsby + Netlify CMS Starter",
+    title: "Gatsby + Netlify CMS Starter",
     description:
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
   },
